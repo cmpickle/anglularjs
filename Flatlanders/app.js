eec837e2acca13ee159a4dc8dc66db4322d4b0bc
Cameron Pickle <cpickle@dealersocket.com>
2018-04-17 14:09:41 -0600

    add images, tabs, and panel controller

--- Flatlanders/app.js
+++ Flatlanders/app.js
@@ -5,19 +5,37 @@
         this.products = gems;
     });
 
+    app.controller('PanelController', function() {
+        this.tab = 1;
+
+        this.selectTab = function(setTab) {
+            this.tab = setTab;
+        };
+
+        this.isSelected = function(checkTab) {
+            return this.tab === checkTab;
+        };
+    });
+
     var gems = [
         {
             name: "Dodecahedron",
             price: 2,
             description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
             canPurchase: true,
-            soldOut: false
+            soldOut: false,
+            images: [
+                "images/gem-01.gif"
+            ]
         },
         {
             name: "Pentagonal Gem",
             price: 5.95,
             description: "5 sided gem",
-            canPurchase: false
+            canPurchase: false,
+            images: [
+                "images/gem-02.gif"
+            ]
         }
     ];
 })();
