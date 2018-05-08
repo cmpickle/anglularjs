249ced4ad5e37623220ff3e5e17a4ec9547f7bcf
Cameron Pickle <cmpickle@gmail.com>
2018-05-08 16:01:17 -0600

    start to angularjs firefamily client

---
+++ firefamily/wwwroot/scripts/app.js
@@ -0,0 +1,21 @@
+(function() {
+    angular.module('firefamily', ['ngRoute'])
+        .config(['$routeProvider', function($routeProvider) {
+            $routeProvider
+                .when('/', {
+                    redirectTo: '/home'
+                })
+                .when('/home', {
+                    templateUrl: '/wwwroot/templates/index.html'
+                });
+        }]);
+
+    angular.module('firefamily').controller("ProductController", ['$http', function($http) {
+        let controller = this;
+
+        $http({ method: 'GET', url: 'http://firefamily.dynu.net:8000/product'})
+            .then(function(data) {
+                controller.products = data.data;
+            });
+    }]);
+})()
