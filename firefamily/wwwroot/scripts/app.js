f2f33de00f008c680828f1c4f2565061354bd34e
Cameron Pickle <cmpickle@gmail.com>
2018-05-18 15:45:21 -0600

    add edit and delete functionality

--- firefamily/wwwroot/scripts/app.js
+++ firefamily/wwwroot/scripts/app.js
@@ -22,23 +22,48 @@
                 })
                 .when('/new-product', {
                     templateUrl: '/wwwroot/templates/new-product.html'
+                })
+                .when('/edit-product/:sku', {
+                    templateUrl: '/wwwroot/templates/edit-product.html'
                 });
         }]);
 
-    angular.module('firefamily').controller("ProductController", ['$http', '$scope', '$location', '$window', function($http, $scope, $location, $window) {
+    angular.module('firefamily').controller("ProductController", ['$http', '$scope', '$location', '$window', '$routeParams', function($http, $scope, $location, $window, $routeParams) {
         let controller = this;
         $scope.search = $location.$$search.search;
         controller.product = {};
+
+        controller.setProduct = function(sku) {
+            $http({ method: 'GET', url: 'http://firefamily.dynu.net:8000/product/' + sku})
+                .then(function(data) {
+                    controller.product = data.data[0];
+                }, function(data) {
+                    alert("Failed to retrieve product SKU = " + sku);
+                });
+        };
+        
+        if($routeParams.sku) {
+            controller.setProduct($routeParams.sku);
+        }
+
+        controller.getProduct = function() {
+            let test = controller.product;
+            return test;
+        };
 
         controller.searchFunc = function(newVal) {
             $location.search("search", newVal);
         };
 
+        controller.loadEdit = function(product) {
+            controller.product = product;
+            $window.location.href = '/#!/edit-product/' + product.sku;
+        };
+
         $http({ method: 'GET', url: 'http://firefamily.dynu.net:8000/product'})
             .then(function(data) {
                 controller.products = data.data;
-            });
-
+        });
 
         controller.createProduct = function(product) {
             controller.product = product;
@@ -47,6 +72,19 @@
                     $scope.message = data;
                 }, function errorCallback(data, status, headers, config) {
                     alert("Failed to create product!\n" + JSON.stringify({data: data}));
+                });
+            $http({ method: 'POST', url: 'http://firefamily.dynu.net:8000/inventory/update/' + product.sku + '/' + product.quantity})
+            controller.product = {};
+            $window.location.href = '/#!/home';
+        }
+
+        controller.edit = function(product) {
+            controller.product = product;
+            $http({ method: 'POST', url: 'http://firefamily.dynu.net:8000/product/update/' + product.sku, headers: {'Content-Type' : 'text/plain'}, data: controller.product})
+                .then(function successCallback(data, status, headers, config) {
+                    $scope.message = data;
+                }, function errorCallback(data, status, headers, config) {
+                    alert("Failed to edit product!\n" + JSON.stringify({data: data}));
                 });
             $http({ method: 'POST', url: 'http://firefamily.dynu.net:8000/inventory/update/' + product.sku + '/' + product.quantity})
             controller.product = {};
@@ -68,6 +106,15 @@
                     $scope.message = data;
                 }, function(data, status, headers, config) {
                     alert("Falied to increment!\n" + JSON.stringify({data:data}));
+                });
+        }
+
+        controller.delete = function(product) {
+            $http({ method: 'POST', url: 'http://firefamily.dynu.net:8000/product/delete/' + product.sku})
+                .then(function(data, status, headers, config) {
+                    $scope.message = data;
+                }, function(data, status, headers, config) {
+                    alert("Failed to delete product!\n" + JSON.stringify({data:data}));
                 });
         }
     }]);
