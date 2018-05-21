7b1fb2e776fc6d6eaea04e10c0ba31084c73489b
Cameron Pickle <cmpickle@gmail.com>
2018-05-21 17:11:58 -0600

    url changes

--- firefamily/wwwroot/scripts/app.js
+++ firefamily/wwwroot/scripts/app.js
@@ -45,14 +45,14 @@
 
         controller.scan = function (sku) {
             if (scannerOpt === "increment") {
-                $http({ method: 'POST', url: 'http://firefamily.dynu.net:8000/inventory/increment/' + sku })
+                $http({ method: 'POST', url: 'http://10.0.0.200:8000/inventory/increment/' + sku })
                     .then(function(data) {
                         controller.message = data;
                     }, function(data) {
                         alert("Failed to increment sku: " + sku + "\n" + JSON.stringify({data: data}));
                     });
             } else {
-                $http({ method: 'POST', url: 'http://firefamily.dynu.net:8000/inventory/decrement/' + sku })
+                $http({ method: 'POST', url: 'http://10.0.0.200:8000/inventory/decrement/' + sku })
                     .then(function(data) {
                         controller.message = data;
                     }, function(data) {
@@ -66,13 +66,13 @@
         $("#sku").focus();
     }]);
 
-    angular.module('firefamily').controller("ProductController", ['$http', '$scope', '$location', '$window', '$routeParams', function ($http, $scope, $location, $window, $routeParams) {
+    angular.module('firefamily').controller("ProductController", ['$http', '$scope', '$location', '$window', '$routeParams', '$route', function ($http, $scope, $location, $window, $routeParams, $route) {
         let controller = this;
         $scope.search = $location.$$search.search;
         controller.product = {};
 
         controller.setProduct = function (sku) {
-            $http({ method: 'GET', url: 'http://firefamily.dynu.net:8000/product/' + sku })
+            $http({ method: 'GET', url: 'http://10.0.0.200:8000/product/' + sku })
                 .then(function (data) {
                     controller.product = data.data[0];
                 }, function (data) {
@@ -98,57 +98,59 @@
             $window.location.href = '/#!/edit-product/' + product.sku;
         };
 
-        $http({ method: 'GET', url: 'http://firefamily.dynu.net:8000/product' })
+        $http({ method: 'GET', url: 'http://10.0.0.200:8000/product' })
             .then(function (data) {
                 controller.products = data.data;
             });
 
         controller.createProduct = function (product) {
             controller.product = product;
-            $http({ method: 'POST', url: 'http://firefamily.dynu.net:8000/product/create', headers: { 'Content-Type': 'text/plain' }, data: controller.product })
+            $http({ method: 'POST', url: 'http://10.0.0.200:8000/product/create', headers: { 'Content-Type': 'text/plain' }, data: controller.product })
                 .then(function successCallback(data, status, headers, config) {
                     $scope.message = data;
                 }, function errorCallback(data, status, headers, config) {
                     alert("Failed to create product!\n" + JSON.stringify({ data: data }));
                 });
-            $http({ method: 'POST', url: 'http://firefamily.dynu.net:8000/inventory/update/' + product.sku + '/' + product.quantity })
+            $http({ method: 'POST', url: 'http://10.0.0.200:8000/inventory/update/' + product.sku + '/' + product.quantity })
             controller.product = {};
             $window.location.href = '/#!/home';
         }
 
         controller.edit = function (product) {
             controller.product = product;
-            $http({ method: 'POST', url: 'http://firefamily.dynu.net:8000/product/update/' + product.sku, headers: { 'Content-Type': 'text/plain' }, data: controller.product })
+            $http({ method: 'POST', url: 'http://10.0.0.200:8000/product/update/' + product.sku, headers: { 'Content-Type': 'text/plain' }, data: controller.product })
                 .then(function successCallback(data, status, headers, config) {
                     $scope.message = data;
                 }, function errorCallback(data, status, headers, config) {
                     alert("Failed to edit product!\n" + JSON.stringify({ data: data }));
                 });
-            $http({ method: 'POST', url: 'http://firefamily.dynu.net:8000/inventory/update/' + product.sku + '/' + product.quantity })
+            $http({ method: 'POST', url: 'http://10.0.0.200:8000/inventory/update/' + product.sku + '/' + product.quantity })
             controller.product = {};
             $window.location.href = '/#!/home';
         }
 
         controller.increment = function (product) {
-            $http({ method: 'POST', url: 'http://firefamily.dynu.net:8000/inventory/increment/' + product.sku, headers: { 'Content-Type': 'applicatoin/json' } })
+            $http({ method: 'POST', url: 'http://10.0.0.200:8000/inventory/increment/' + product.sku, headers: { 'Content-Type': 'applicatoin/json' } })
                 .then(function (data, status, headers, config) {
                     $scope.message = data;
                 }, function (data, status, headers, config) {
                     alert("Falied to increment!\n" + JSON.stringify({ data: data }));
                 });
+            $route.reload();
         }
 
         controller.decrement = function (product) {
-            $http({ method: 'POST', url: 'http://firefamily.dynu.net:8000/inventory/decrement/' + product.sku, headers: { 'Content-Type': 'application/json' } })
+            $http({ method: 'POST', url: 'http://10.0.0.200:8000/inventory/decrement/' + product.sku, headers: { 'Content-Type': 'application/json' } })
                 .then(function (data, status, headers, config) {
                     $scope.message = data;
                 }, function (data, status, headers, config) {
                     alert("Falied to increment!\n" + JSON.stringify({ data: data }));
                 });
+            $route.reload()
         }
 
         controller.delete = function (product) {
-            $http({ method: 'POST', url: 'http://firefamily.dynu.net:8000/product/delete/' + product.sku })
+            $http({ method: 'POST', url: 'http://10.0.0.200:8000/product/delete/' + product.sku })
                 .then(function (data, status, headers, config) {
                     $scope.message = data;
                 }, function (data, status, headers, config) {
